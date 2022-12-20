981f138a95165c1fa269b4fa83c3f7ae9f8b6896
Yana Peshko <yanapeshko917@gmail.com>
2022-12-19 20:43:49 -0700

    adding

---
+++ App.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  Alert,
+  Button,
+} from "react-native";
+import * as Font from "expo-font";
+
+export default function App() {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const nameHandler = (text) => setName(text);
+  const passwordHandler = (text) => setPassword(text);
+
+  const onLogin = () => {
+    Alert.alert("Credentials", `${name} + ${password}`);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS == "ios" ? "padding" : "height"}
+        >
+          <TextInput
+            value={name}
+            onChangeText={nameHandler}
+            placeholder="Username"
+            style={styles.input}
+          />
+          <TextInput
+            value={password}
+            onChangeText={passwordHandler}
+            placeholder="Password"
+            secureTextEntry={true}
+            style={styles.input}
+          />
+          <Button title={"Login"} style={styles.input} onPress={onLogin} />
+        </KeyboardAvoidingView>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 10,
+  },
+});
